3ab4e7100ceed9df1604b041c979a328fc1833bb
doghap <doghap@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-11 07:11:27 +0000

    new_version
    git-svn-id: https://plugins.svn.wordpress.org/wp-uighur-fonts/trunk@1132559 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ug.php
+++ ug.php
@@ -2,25 +2,54 @@
 /*
 Plugin Name: WP-Uighur-Fonts
 Plugin URI: http://doghap.net/wp-uighur-fonts.html
-Description: WordPress نىڭ ئۇيغۇرچە نەشىرى ئۈچۈن ئۇيغۇرچە فونت ۋە كىرگۈزگۈچ تەمىنلەيدۇ.  WordPress 3.8 ئۇيغۇرچە نەشىرى ۋە شۇنىڭدىن يۇقىرى نەشىرلىرىگە ماس كېلىدۇ.
+Description: WordPress نىڭ ئۇيغۇرچە نەشىرى ئۈچۈن ئۇيغۇرچە فونت ۋە كىرگۈزگۈچ تەمىنلەيدۇ.  WordPress 3.8 ئۇيغۇرچە نەشىرى ۋە شۇنىڭدىن يۇقىرى نەشىرلىرىگە ماس كېلىدۇ.سىز <code>ئىشلەتكۈچىلەر »» ئارخىبىڭىز </code>دېگەن يەردىن خەت نۇسقىسى تاللىيالايسىز. ئالدى ۋە ئارقا سەھنىدە بىۋاستە ئۇيغۇرچە كىرگۈزەلەيسىز.
 Author: Doghap.Net(دوغاپ)
-Version: 1.0.1
+Version: 1.1.0
 Author URI: http://www.doghap.net
 */
 
-add_action('admin_init','ug_vk');
-add_action('init','ug_vk');
-add_action('admin_init','fontfaces_css');
-add_action('wp_enqueue_scripts', 'remove_fontFaces');
-add_action('login_enqueue_scripts', 'fontfaces_css');
-add_filter( 'mce_css', 'mce_new_css' );
+/* 
+ * Register settings 
+ */
+add_action( 'admin_init', 'my_register_settings' );
+function my_register_settings() 
+{
+    register_setting( 
+        'general', 
+        'use_frontend_font',
+        'esc_html' // <--- Customize this if there are multiple fields
+    );
+    add_settings_section( 
+        'frontend-font', 
+        'ئالدى بەت خەت نۇسقىسى', 
+        '__return_false', 
+        'general' 
+    );
+    add_settings_field( 
+        'use_frontend_font', 
+        'ئالدى سەھنىگە خەت نۇسقىسى ئىشلىتىش', 
+        'add_my_settings_field', 
+        'general', 
+        'frontend-font' 
+    );
+}    
+
+/* 
+ * Print settings field
+ */
+function add_my_settings_field() {
+    $value = html_entity_decode( get_option( 'use_frontend_font' ) );
+    $checked = ($value=='yes') ? 'checked="checked"' : '';
+    echo '<input type="hidden" name="use_frontend_font" value="no" /><input id="clinto_under_maintenance" type="checkbox" '.$checked.' name="use_frontend_font" value="yes" />';
+}
+
+/*
+ * print user custom field
+ */
 add_action('show_user_profile', 'user_font_family',3, 1);
 add_action('edit_user_profile', 'user_font_family',3, 1);
-add_action( 'profile_update', 'my_font_family_update', 10, 2 );
-
 function user_font_family() {
 ?>
-
 <table class="form-table">
   <tr>
     <th> <label for="tc_location">خەت نۇسخىسى</label>
@@ -37,6 +66,10 @@ function user_font_family() {
 <?php
 }
 
+/* 
+ * update user option
+ */
+add_action( 'profile_update', 'my_font_family_update', 10, 2 );
 function my_font_family_update($user_id, $old_user_data ) {
 	$font_family = $_POST['user_font_family'];
 	if(get_the_author_meta( 'user_font_family', $user_id ) !=$font_family){
@@ -44,36 +77,65 @@ function my_font_family_update($user_id, $old_user_data ) {
 	}
 }
 
+/* 
+ * add ug_vk script 
+ */
+add_action('admin_init','ug_vk');
+add_action('init','ug_vk');
 function ug_vk() {
 	wp_register_script('ug_vk', plugins_url('includes/ug_vk.js', __FILE__));
 	wp_enqueue_script('ug_vk');
 }
 
+/*
+ * romove the font-face
+ */
+add_action('wp_enqueue_scripts', 'remove_fontFaces');
 function remove_fontFaces()
 {
 	wp_dequeue_style(array('fontFaces', 'fontFaces-css'));
 	wp_deregister_style( array('fontFaces', 'fontFaces-css') );
 }
 
-function fontfaces_css () {
+/*
+ * register frontend css
+ */
+$value = html_entity_decode( get_option( 'use_frontend_font' ) );
+if($value == 'yes'){add_action('wp_enqueue_scripts','fontface_css_frontend');}
+function fontface_css_frontend () {
 	if(esc_attr( get_the_author_meta( 'user_font_family', get_current_user_ID() ) ) == 'UKIJ Tuz Tom'){
-		wp_enqueue_style('ug_vk', plugins_url('includes/font2.css', __FILE__));
+		wp_enqueue_style('ug_frontend_font_css', plugins_url('includes/font_frontend_2.css', __FILE__));
 	}else{
-		wp_enqueue_style('ug_vk', plugins_url('includes/font.css', __FILE__));
+		wp_enqueue_style('ug_frontend_font_css', plugins_url('includes/font_frontend.css', __FILE__));
 	}
 }
 
+/*
+ * register backend css
+ */
+add_action('admin_init','fontface_css_beckend');
+add_action('login_enqueue_scripts', 'fontface_css_beckend');
+function fontface_css_beckend () {
+	if(esc_attr( get_the_author_meta( 'user_font_family', get_current_user_ID() ) ) == 'UKIJ Tuz Tom'){
+		wp_enqueue_style('ug_beckend_font_css', plugins_url('includes/ug_beckend_font_2.css', __FILE__));
+	}else{
+		wp_enqueue_style('ug_beckend_font_css', plugins_url('includes/ug_beckend_font.css', __FILE__));
+	}
+}
+
+/*
+ * add editor style
+ */
+add_filter( 'mce_css', 'mce_new_css' );
 function mce_new_css($mce_css) {
   if (! empty($mce_css)) $mce_css .= ',';
-  
-  
+
   if(esc_attr( get_the_author_meta( 'user_font_family', get_current_user_ID() ) ) == 'UKIJ Tuz Tom'){
-		$mce_css .= plugins_url('includes/font2.css', __FILE__);
+		$mce_css .= plugins_url('includes/font_backtend_2.css', __FILE__);
 	}else{
-		$mce_css .= plugins_url('includes/font.css', __FILE__);
+		$mce_css .= plugins_url('includes/font_backtend.css', __FILE__);
 	}
   
   return $mce_css;
 }
-
 ?>
